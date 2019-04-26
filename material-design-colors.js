c6b2873a735e4dce4db900232d5e2ff2a199a24e
Owen Choi <myartame@gmail.com>
2019-04-26 17:29:33 +0900

    [ADD] material-design-colors theme(for blink)

---
+++ material-design-colors.js
@@ -0,0 +1,27 @@
+black       = '#546D79';
+red         = '#FF5151'; // red
+green       = '#69F0AD'; // green
+yellow      = '#FFD73F'; // yellow
+blue        = '#40C4FE'; // blue
+magenta     = '#FF3F80'; // pink
+cyan        = '#64fcda'; // cyan
+white       = '#fefefe'; // light gray
+lightBlack  = '#b0bec4'; // medium gray
+lightRed    = '#ff8980'; // red
+lightGreen  = '#b9f6c9'; // green
+lightYellow = '#ffe47e'; // yellow
+lightBlue   = '#80d7fe'; // blue
+lightMagenta= '#ff80ab'; // pink
+lightCyan   = '#a7fdeb'; // cyan
+lightWhite  = '#fefefe'; // white
+
+t.prefs_.set('color-palette-overrides',
+                 [ black , red     , green  , yellow,
+                  blue     , magenta , cyan   , white,
+                  lightBlack   , lightRed  , lightGreen , lightYellow,
+                  lightBlue    , lightMagenta  , lightCyan  , lightWhite ]);
+
+t.prefs_.set('cursor-color', 'rgba(0, 0, 0, 0.5)');
+t.prefs_.set('foreground-color', '#eceef0');
+t.prefs_.set('background-color', '#263137');
+
