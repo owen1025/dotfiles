d07333de6f3866f68dd88b7d9427656a097a70b0
Gowid <gowid@Gowidui-MacBookPro.local>
2026-04-10 22:25:21 +0900

    feat(chezmoi): add Homebrew bootstrap script

---
+++ run_before_00-install-homebrew.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+set -e
+
+if ! command -v brew &>/dev/null; then
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    eval "$(/opt/homebrew/bin/brew shellenv)"
+fi
